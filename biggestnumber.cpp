497f1f3c4cd8fca19a685d9e68f75f38246fe353
pvang40 <pvang40@mail.csuchico.edu>
2018-02-05 17:32:58 -0800

    Stores input values

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -5,10 +5,13 @@ using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-  
+  int A;
   cout<<"Please enter a whole number:\n";
-  
+  cin>> A;
+
+  int B;
   cout<<"Please enter another whole number:\n";
+  cin>> B;
 
   cout<<"Of those two numbers, the biggest is: ";
   cout<<endl<<"Thank you for playing.\n";
